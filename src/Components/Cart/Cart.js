ce07553e02a55cad7572cf2540a4ce88f42efc94
Pobarul-islam <pobarul3@gmail.com>
2022-04-01 19:43:44 +0600

    logical operator added

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -2,6 +2,25 @@ import React from 'react';
 import './Cart.css';
 
 const Cart = ({ cart, handleRemoveFromCart }) => {
+    // Conditional rendering options
+    // 1. Element variable
+    // 2. Ternary operator condition ? true: false
+    // 3. && operator  (shorthand)
+    // 4.  ||
+    let command;
+    if (cart.length === 0) {
+        command = <p>Please Add at list one item!!!</p>
+    }
+    else if (cart.length === 1) {
+        command = <div>
+            <h5>Ohh kipta, khoroch kor</h5>
+            <p>kinos na ken</p>
+
+        </div>
+    }
+    else {
+        command = <p><small>Thanks for adding item</small></p>
+    }
   return (
     <div>
       <h2>Items Selected: {cart.length}</h2>
@@ -10,7 +29,15 @@ const Cart = ({ cart, handleRemoveFromCart }) => {
           {tShirt.name}
           <button onClick={() => handleRemoveFromCart(tShirt)}>X</button>
         </p>
-      ))}
+      ))};
+      {cart.length === 0 || <p className='orange'>YAY ! You are buying</p>}
+          {cart.length === 3 && <div className='orange'>
+              <h3>Trigonal</h3>
+              <p>Tin jon ke ki diba</p>
+          </div>}
+          {command}
+          {cart.length !== 4 ? <p>Keep adding</p> : <button>Remove All</button>}
+          {cart.length ===4 && <button className='orange'>Review order</button>}
     </div>
   );
 };
